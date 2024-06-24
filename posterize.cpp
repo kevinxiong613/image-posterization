38b0b7c01d32c5d54724d550c12184308940e4e0
Kevin <kevxemail@gmail.com>
2024-06-23 20:37:38 -0400

    Created file for cpp equivalent program to begin refactoring to c++ with opencv

---
+++ posterize.cpp
@@ -0,0 +1,9 @@
+// g++ -pedantic-errors -ggdb -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Werror -std=c++20 -o programName programName.cpp
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Test";
+    return 0;
+}
